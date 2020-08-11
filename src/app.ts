b89a9201ff95ea3f91ef20e77b48c8fb1d215632
Jon Deaves <hello@jondeaves.me>
2020-08-11 23:30:51 +0100

    chore: a bit of documentation and cleanup

--- src/app.ts
+++ src/app.ts
@@ -49,53 +49,6 @@ export default class App {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
       }
-
-
-
-      // Will trigger anytime a message is sent in channel
-      // if (command === 'ping') {
-      //   message.reply('pong');
-      // } else if (command === 'see') {
-      //   message.reply(`Server: ${message.guild.name}\nYour username: ${message.author.username}\nYour ID: ${message.author.id}`);
-      // } else if (command === 'kick') {
-      //   // grab the "first" mentioned user from the message
-      //   // this will return a `User` object, just like `message.author`
-      //   if (!message.mentions.users.size) {
-      //     return message.reply('you need to tag a user in order to kick them!');
-      //   }
-
-      //   const taggedUser = message.mentions.users.first();
-
-      //   message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-      // } else if (command === 'avatar') {
-      //   if (!message.mentions.users.size) {
-      //     return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
-      //   }
-
-      //   const avatarList = message.mentions.users.map(user => {
-      //     return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
-      //   });
-
-      //   // send the entire array of strings as a message
-      //   // by default, discord.js will `.join()` the array with `\n`
-      //   message.channel.send(avatarList);
-      // } else if (command === 'prune') {
-      //   const amount = parseInt(args[0]) + 1;
-
-      //   if (isNaN(amount)) {
-      //     return message.reply('that doesn\'t seem to be a valid number.');
-      //   } else if (amount <= 1 || amount > 100) {
-      //     return message.reply('you need to input a number between 2 and 99.');
-      //   }
-
-      //   message.channel.bulkDelete(amount, true).catch((reason) => {
-      //     if (reason.code === 50013) {
-      //       message.reply('I don\'t have permission to do that.')
-      //     } else {
-      //       message.reply(`I couldn't do that because: ${reason.message}`)
-      //     }
-      //   });
-      // }
     });
 
     this._discordClient.login(this._configService.get('DISCORD_BOT_TOKEN'))
