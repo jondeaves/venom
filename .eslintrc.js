a8dbd0c1acfef42c4f69c7bc4473a74dfb07d96f
Jon Deaves <jondeaves@users.noreply.github.com>
2020-08-14 19:32:14 +0100

    test: add testing framework with sample test

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,17 @@ module.exports = {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
   },
-  plugins: [
-    "@typescript-eslint",
-    "eslint-comments",
-    "jest",
-    "promise",
-    "unicorn",
-  ],
+  plugins: ['@typescript-eslint', 'eslint-comments', 'jest', 'promise', 'unicorn'],
   extends: [
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:jest/recommended",
-    "plugin:promise/recommended",
-    "plugin:unicorn/recommended",
-    "prettier",
-    "prettier/react",
-    "prettier/@typescript-eslint",
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:jest/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
   ],
   env: {
     node: true,
@@ -28,32 +22,37 @@ module.exports = {
   },
   rules: {
     // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
+    'no-prototype-builtins': 'off',
     // https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "off",
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'off',
     // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
+    'react/destructuring-assignment': 'off',
     // No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
-    "react/jsx-filename-extension": "off",
+    'react/jsx-filename-extension': 'off',
     // Use function hoisting to improve code readability
-    "no-use-before-define": [
-      "error",
-      { functions: false, classes: true, variables: true },
-    ],
+    'no-use-before-define': ['error', { functions: false, classes: true, variables: true }],
     // Makes no sense to allow type inferrence for expression parameters, but require typing the response
-    "@typescript-eslint/explicit-function-return-type": [
-      "error",
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
       { allowExpressions: true, allowTypedFunctionExpressions: true },
     ],
-    "@typescript-eslint/no-use-before-define": [
-      "error",
+    '@typescript-eslint/no-use-before-define': [
+      'error',
       { functions: false, classes: true, variables: true, typedefs: true },
     ],
     // Common abbreviations are known and readable
-    "unicorn/prevent-abbreviations": "off",
-    "unicorn/filename-case": "off",
-    "unicorn/no-fn-reference-in-iterator": "off",
-    "no-underscore-dangle": ["error", { "allowAfterThis": true }]
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/no-fn-reference-in-iterator': 'off',
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
   },
-}
+  overrides: [
+    {
+      files: ['*.spec.ts'],
+      rules: {
+        'jest/valid-expect': 0,
+      },
+    },
+  ],
+};
--- src/core/services/config.service.ts
+++ src/core/services/config.service.ts
@@ -1,6 +1,4 @@
-import dotenv from 'dotenv';
 import { injectable } from 'inversify';
-import path from 'path';
 
 import Config from '../types/Config';
 import Environment from '../types/Environment';
@@ -17,22 +15,16 @@ export default class ConfigService {
   private config: Config;
 
   public get isProd(): boolean {
-    return this.get('ENVIRONMENT') === 'production';
+    return this.get('NODE_ENV') === 'production';
   }
 
   constructor() {
-    dotenv.config({ path: path.resolve(__dirname, '../../', '.env') });
-
-    this.setup();
-  }
-
-  private setup(): void {
     this.config = {
       BOT_TRIGGER: process.env.BOT_TRIGGER || '!',
       DISCORD_BOT_TOKEN: process.env.DISCORD_BOT_TOKEN,
       MONGODB_URI: process.env.MONGODB_URI || '',
       MONGODB_DB_NAME: process.env.MONGODB_DB_NAME || '',
-      ENVIRONMENT: (process.env.NODE_ENV as Environment) || 'development',
+      NODE_ENV: (process.env.NODE_ENV as Environment) || 'development',
       LOG_LEVEL: (process.env.LOG_LEVEL as LogLevel) || 'info',
     };
   }
