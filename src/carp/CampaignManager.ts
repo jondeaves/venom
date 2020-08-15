1df3166684123be9f6133f2bfdfd282abedf2ff6
Lance Krasniqi <kaizoku-lance@hotmail.com>
2020-08-15 17:09:06 +0200

    feat: added a stop function for moderators to stop a campaign

--- src/carp/CampaignManager.ts
+++ src/carp/CampaignManager.ts
@@ -7,6 +7,7 @@ import DatabaseService from '../core/services/database.service';
 import LoggerService from '../core/services/logger.service';
 
 import Campaign from './campaign/campaign.entity';
+import Character from './character/character.entity';
 
 export default class CampaignManager {
   private _configService: ConfigService = container.resolve<ConfigService>(ConfigService);
@@ -27,6 +28,7 @@ export default class CampaignManager {
 
   public async execute(message: Message): Promise<void> {
     const prefix = this._configService.get('CAMPAIGN_TRIGGER');
+    const moderatorPermission = message.member.roles.cache.has('743465073611243662');
 
     if (!message.content.toLowerCase().startsWith(prefix)) {
       return;
@@ -40,10 +42,33 @@ export default class CampaignManager {
         default:
           message.reply(`I don't recognize that command. Try again or type \`help\` for a list of commands.`);
           break;
-        case 'status':
+        case 'status': {
           message.channel.send(`Welcome to campaign ${this._campaign.id}`);
           message.channel.send(`There are ${this._campaign.characters.length} weary travellers.`);
           break;
+        }
+        case 'stop':
+          if (moderatorPermission) {
+            this._databaseService.manager.delete(Campaign, this._campaign.id);
+            message.channel.send(`The campaign has ended!`);
+          } else {
+            message.reply(
+              `unfortunately you do not have permission to run that command. Contact a moderator to discuss your intentions.`,
+            );
+          }
+          break;
+        case 'leave': {
+          const matchedCharIndex = this._campaign.characters.findIndex((char) => char.uid === message.author.id);
+          const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+          if (matchedCharIndex >= 0) {
+            this._campaign.characters.splice(matchedCharIndex, 1);
+            this._databaseService.manager.save(this._campaign);
+            message.reply(`your character **${matchedChar.name}** has left the campaign.`);
+            return;
+          }
+          message.reply(`I couldn't find your character. Reach out to a moderator to help you out with this issue.`);
+          break;
+        }
       }
     }
   }
