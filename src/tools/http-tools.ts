ec19f283a94ae8b4679eb8d05c985f2f9dacbca6
Chen Asraf <chen@asraf.me>
2020-08-15 22:20:09 +0300

    chore: cleanup

--- src/tools/http-tools.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable eslint-comments/disable-enable-pair */
-/* eslint-disable import/no-extraneous-dependencies */
-import axios, { AxiosRequestConfig } from 'axios';
-import cheerio from 'cheerio';
-
-export async function getHTMLPage(url: string, config?: AxiosRequestConfig): Promise<CheerioStatic> {
-  const res = await axios.get(url, config);
-  return cheerio.load(res.data.toString());
-}
