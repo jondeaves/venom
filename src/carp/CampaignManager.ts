02819a69b5b737448b2ab0165d26e326569b34af
Lance Krasniqi <kaizoku-lance@hotmail.com>
2020-08-16 04:29:03 +0200

    feat: added examine command to look at specific objects or persons

--- src/carp/CampaignManager.ts
+++ src/carp/CampaignManager.ts
@@ -155,6 +155,7 @@ export default class CampaignManager {
             const msg = [];
 
             const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+            const allChar = await this._databaseService.manager.find(Character);
             const myPos = JSON.parse(matchedChar.position);
 
             const sight = args[1] && args[1] === 'all' ? -1 : 3;
@@ -164,20 +165,30 @@ export default class CampaignManager {
               let diffX = 0;
               let diffY = 0;
               for (let j = 0; j < map.world[i].length; j += 1) {
-                if (myPos.x === j && myPos.y === i) {
-                  line += ':bust_in_silhouette:';
-                  j += 1;
+                let playerHere = false;
+                let isMe = false;
+                // eslint-disable-next-line no-restricted-syntax
+                for (const char of allChar) {
+                  playerHere = this.isPlayerHere(char, j, i);
+                  if (playerHere) {
+                    isMe = char.name === matchedChar.name;
+                    break;
+                  }
                 }
                 diffX = Math.abs(j - myPos.x);
                 diffY = Math.abs(i - myPos.y);
                 if ((diffX < sight && diffY < sight) || sight === -1) {
+                  if (playerHere) {
+                    line += isMe ? ':slight_smile:' : ':neutral_face:';
+                    j += 1;
+                  }
                   if (map.world[i][j] === 0) line += ':white_large_square:';
                   if (map.world[i][j] === 1) line += ':black_large_square:';
                   if (map.world[i][j] === 2) line += ':white_large_square:';
                   if (map.world[i][j] === 3) line += ':door:';
                   if (map.world[i][j] === 4) line += ':door:';
-                  if (map.world[i][j] === 5) line += ':green_square:';
-                  if (map.world[i][j] === 6) line += ':star:';
+                  if (map.world[i][j] === 5) line += ':checkered_flag:';
+                  if (map.world[i][j] === 6) line += ':triangular_flag_on_post:';
                 }
               }
               msg.push(line);
@@ -192,42 +203,107 @@ export default class CampaignManager {
           message.channel.send(`> **${matchedChar.name}** looks around.`);
 
           const list = [];
-          const surroundings = this.getSurroundings(map.world, myPos.x, myPos.y, 3);
+          const surroundings = await this.getSurroundings(map.world, myPos.x, myPos.y, 4, message.author.id);
           surroundings.forEach((element) => {
-            switch (element) {
-              case 3:
-              case 4:
-                list.push('a door');
-                break;
-              default:
-                break;
-            }
+            list.push(`**${element}**`);
           });
           if (list.length === 0) {
             message.channel.send(`> **${matchedChar.name}** sees nothing of note.`);
           } else if (list.length === 1) {
             message.channel.send(`> **${matchedChar.name}** sees ${list[0]}.`);
           } else {
-            message.channel.send(`> **${matchedChar.name}** sees ${list.join(' and ').slice(0, 4)}.`);
+            message.channel.send(`> **${matchedChar.name}** sees ${list.join(' and ')}.`);
           }
           break;
         }
+        case 'examine':
+          if (args[1]) {
+            const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+            const map = JSON.parse(this._campaign.dungeon);
+            const myPos = JSON.parse(matchedChar.position);
+            const surroundings = await this.getSurroundings(map.world, myPos.x, myPos.y, 4, message.author.id);
+            const nextArgs = args.splice(1).join(' ');
+            if (surroundings.find((obj) => obj === nextArgs)) {
+              const allChar = await this._databaseService.manager.find(Character);
+              const char = this.isPlayer(allChar, nextArgs);
+              if (char) {
+                const examine = await this._databaseService.manager.findOne(Character, char);
+                message.channel.send(`> **${matchedChar.name}** examines **${examine.name}**.`);
+                const extraInfo = [];
+                if (examine.power > matchedChar.defense) extraInfo.push('They look pretty powerful.');
+                else if (examine.power === matchedChar.power) extraInfo.push('We look equally strong.');
+                else extraInfo.push("They don't look very strong.");
+                if (examine.defense > matchedChar.power) extraInfo.push('They look pretty defensive.');
+                else if (examine.defense === matchedChar.defense) extraInfo.push('We look equally guarded.');
+                else extraInfo.push("They don't look very shielded.");
+                if (examine.current_health === examine.max_health) extraInfo.push('They look very healthy.');
+                else extraInfo.push("It looks like the're wounded.");
+                if (extraInfo.length > 1) message.channel.send(`> ${extraInfo.join(' ')}`);
+              } else {
+                switch (nextArgs) {
+                  default:
+                    message.channel.send(`> There doesn't seem anything to be here.`);
+                    break;
+                  case 'door':
+                    message.channel.send(`> The door seems accessile.`);
+                    break;
+                  case 'start flag':
+                    message.channel.send(`> That's the start flag, it is where we all started in this crazy place.`);
+                    break;
+                }
+              }
+            } else {
+              message.channel.send(`> **${matchedChar.name}** stares blankly in front of them.`);
+            }
+          }
+          break;
       }
     }
   }
 
-  getSurroundings(world: [[]], x: number, y: number, range: number): number[] {
+  isPlayerHere(char: Character, x: number, y: number): boolean {
+    const pos = JSON.parse(char.position);
+    if (pos.x === x && pos.y === y) return true;
+    return false;
+  }
+
+  isPlayer(chars: Character[], name: string): Character {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const char of chars) {
+      if (char.name === name) {
+        return char;
+      }
+    }
+    // eslint-disable-next-line unicorn/no-null
+    return null;
+  }
+
+  async getSurroundings(world: [[]], x: number, y: number, range: number, uid: string): Promise<string[]> {
     const result = [];
+    const allChar = await this._databaseService.manager.find(Character);
+    const self = await this._databaseService.manager.findOne(Character, uid);
 
     // eslint-disable-next-line unicorn/no-for-loop
     for (let i = 0; i < world.length; i += 1) {
       let diffX = 0;
       let diffY = 0;
+      let player = '';
       for (let j = 0; j < world[i].length; j += 1) {
         diffX = Math.abs(j - x);
         diffY = Math.abs(i - y);
         if ((diffX < range && diffY < range) || range === -1) {
-          if (world[i][j] === 3 || world[i][j] === 4) result.push(world[i][j]);
+          let playerHere = false;
+          // eslint-disable-next-line no-restricted-syntax
+          for (const char of allChar) {
+            if (char.name !== self.name) {
+              playerHere = this.isPlayerHere(char, j, i);
+              player = char.name;
+              if (playerHere) break;
+            }
+          }
+          if (playerHere) result.push(`${player}`);
+          if (world[i][j] === 3 || world[i][j] === 4) result.push('door');
+          if (world[i][j] === 5) result.push('start flag');
         }
       }
     }
