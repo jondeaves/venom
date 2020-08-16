b81743b999e8aa7e140d5d38f25690a22ff515b6
Lance Krasniqi <kaizoku-lance@hotmail.com>
2020-08-16 02:46:50 +0200

    feat: added walking on map using new walk command

--- src/carp/CampaignManager.ts
+++ src/carp/CampaignManager.ts
@@ -71,63 +71,144 @@ export default class CampaignManager {
           message.reply(`I couldn't find your character. Reach out to a moderator to help you out with this issue.`);
           break;
         }
-        case 'map': {
-          let map = JSON.parse(this._campaign.dungeon);
-          if (args[1] && args[1] === 'loc') {
+        case 'walk': {
+          if (args[1]) {
             const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
-            message.channel.send(matchedChar.position);
-            break;
-          }
-          if (args[1] && args[1] === 'new') {
-            if (hasModPermissions) {
-              map = roguelike({
-                width: 25, // Max Width of the world
-                height: 25, // Max Height of the world
-                retry: 100, // How many times should we try to add a room?
-                special: false, // Should we generate a "special" room?
-                room: {
-                  ideal: 8, // Give up once we get this number of rooms
-                  min_width: 3,
-                  max_width: 7,
-                  min_height: 3,
-                  max_height: 7,
-                },
-              });
-
-              this._campaign.dungeon = map;
-              this._databaseService.manager.save(this._campaign);
-
-              message.channel.send(`Alright, here's a new map:`);
-            } else {
-              message.reply(
-                `unfortunately you do not have permission to run that command. Contact a moderator to discuss your intentions.`,
-              );
+            const map = JSON.parse(this._campaign.dungeon);
+            const myPos = JSON.parse(matchedChar.position);
+            switch (args[1]) {
+              default:
+                message.channel.send(`> ${matchedChar.name} remains in place.`);
+                break;
+              case 'n':
+                if (myPos.y - 1 <= 0) {
+                  message.channel.send(`> ${matchedChar.name} cannot pass that way.`);
+                } else {
+                  myPos.y -= 1;
+                  message.channel.send(`> ${matchedChar.name} takes a step, northward.`);
+                }
+                break;
+              case 'e':
+                if (myPos.x + 1 >= map.width) {
+                  message.channel.send(`> ${matchedChar.name} cannot pass that way.`);
+                } else {
+                  myPos.x += 1;
+                  message.channel.send(`> ${matchedChar.name} takes a step, eastward.`);
+                }
+                break;
+              case 'w':
+                if (myPos.x - 1 <= 0) {
+                  message.channel.send(`> ${matchedChar.name} cannot pass that way.`);
+                } else {
+                  myPos.x -= 1;
+                  message.channel.send(`> ${matchedChar.name} takes a step, westward.`);
+                }
+                break;
+              case 's':
+                if (myPos.x + 1 >= map.height) {
+                  message.channel.send(`> ${matchedChar.name} cannot pass that way.`);
+                } else {
+                  myPos.y += 1;
+                  message.channel.send(`> ${matchedChar.name} takes a step, southward.`);
+                }
+                break;
             }
+            matchedChar.position = JSON.stringify(myPos);
+            await this._databaseService.manager.save(matchedChar);
           }
-          const msg = [];
+          break;
+        }
+        case 'map':
+          {
+            let map = JSON.parse(this._campaign.dungeon);
+            if (args[1] && args[1] === 'loc') {
+              const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+              message.channel.send(matchedChar.position);
+              break;
+            }
+            if (args[1] && args[1] === 'new') {
+              if (hasModPermissions) {
+                map = roguelike({
+                  width: 25, // Max Width of the world
+                  height: 25, // Max Height of the world
+                  retry: 100, // How many times should we try to add a room?
+                  special: false, // Should we generate a "special" room?
+                  room: {
+                    ideal: 8, // Give up once we get this number of rooms
+                    min_width: 3,
+                    max_width: 7,
+                    min_height: 3,
+                    max_height: 7,
+                  },
+                });
 
-          const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
-          const myPos = JSON.parse(matchedChar.position);
-          for (let i = 0; i < map.world.length; i += 1) {
-            let line = '';
-            for (let j = 0; j < map.world[i].length; j += 1) {
-              if (myPos.x === j && myPos.y === i) {
-                line += ':bust_in_silhouette:';
-                j += 1;
+                this._campaign.dungeon = map;
+                this._databaseService.manager.save(this._campaign);
+
+                message.channel.send(`Alright, here's a new map:`);
+              } else {
+                message.reply(
+                  `unfortunately you do not have permission to run that command. Contact a moderator to discuss your intentions.`,
+                );
               }
-              if (map.world[i][j] === 0) line += ':white_large_square:';
-              if (map.world[i][j] === 1) line += ':black_large_square:';
-              if (map.world[i][j] === 2) line += ':white_large_square:';
-              if (map.world[i][j] === 3) line += ':door:';
-              if (map.world[i][j] === 4) line += ':door:';
-              if (map.world[i][j] === 5) line += ':green_square:';
-              if (map.world[i][j] === 6) line += ':star:';
             }
-            msg.push(line);
+            const msg = [];
+
+            const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+            const myPos = JSON.parse(matchedChar.position);
+
+            const sight = args[1] && args[1] === 'all' ? -1 : 3;
+
+            for (let i = 0; i < map.world.length; i += 1) {
+              let line = '';
+              let diffX = 0;
+              let diffY = 0;
+              for (let j = 0; j < map.world[i].length; j += 1) {
+                if (myPos.x === j && myPos.y === i) {
+                  line += ':bust_in_silhouette:';
+                  j += 1;
+                }
+                diffX = Math.abs(j - myPos.x);
+                diffY = Math.abs(i - myPos.y);
+                if ((diffX < sight && diffY < sight) || sight === -1) {
+                  if (map.world[i][j] === 0) line += ':white_large_square:';
+                  if (map.world[i][j] === 1) line += ':black_large_square:';
+                  if (map.world[i][j] === 2) line += ':white_large_square:';
+                  if (map.world[i][j] === 3) line += ':door:';
+                  if (map.world[i][j] === 4) line += ':door:';
+                  if (map.world[i][j] === 5) line += ':green_square:';
+                  if (map.world[i][j] === 6) line += ':star:';
+                }
+              }
+              msg.push(line);
+            }
+            message.channel.send(msg, { split: true });
           }
-          message.channel.send(msg, { split: true });
+          break;
+        case 'look': {
+          const matchedChar = await this._databaseService.manager.findOne(Character, message.author.id);
+          message.channel.send(`> ${matchedChar.name} looks around.`);
+          break;
         }
       }
     }
+  }
+
+  static getSurroundings(world: [[]], x: number, y: number, range: number): number[] {
+    const result = [];
+
+    // eslint-disable-next-line unicorn/no-for-loop
+    for (let i = 0; i < world.length; i += 1) {
+      let diffX = 0;
+      let diffY = 0;
+      for (let j = 0; j < world[i].length; j += 1) {
+        diffX = Math.abs(j - x);
+        diffY = Math.abs(i - y);
+        if ((diffX < range && diffY < range) || range === -1) {
+          if (world[i][j] === 3 || world[i][j] === 4) result.push(world[i][j]);
+        }
+      }
+    }
+    return result;
   }
 }
