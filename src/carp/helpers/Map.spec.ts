4aa6f9764b565140e374b25e2b406297fde09443
Lance Krasniqi <kaizoku-lance@hotmail.com>
2020-08-18 16:42:26 +0200

    fix: map unit testing

--- src/carp/helpers/Map.spec.ts
+++ src/carp/helpers/Map.spec.ts
@@ -6,36 +6,54 @@ import Map from './Map';
 describe('Map', () => {
   describe('Undefined', () => {
     it('should return a map with zero values', async () => {
-      const result = new Map(0, 0, undefined, undefined, 0, undefined, undefined);
+      const result = new Map(
+        0,
+        0,
+        { x: 0, y: 0, room_id: 0 },
+        { x: 0, y: 0, room_id: 0 },
+        0,
+        { '0': { neighbors: [], doors: [], top: 0, left: 0, width: 0, height: 0, deadend: true } },
+        [[]],
+      );
 
-      expect(result.isNull).to.deep.equal(true);
+      expect(result.width).to.deep.equal(0);
+      expect(result.height).to.deep.equal(0);
     });
   });
 
   describe('toJSON', () => {
     it('should turn Map into string', async () => {
-      const map = new Map(1, 1, undefined, undefined, 0, undefined, undefined);
+      const map = new Map(
+        1,
+        1,
+        { x: 0, y: 0, room_id: 0 },
+        { x: 0, y: 0, room_id: 0 },
+        0,
+        { '0': { neighbors: [], doors: [], top: 0, left: 0, width: 0, height: 0, deadend: true } },
+        [[]],
+      );
       const result = map.toJSON();
 
       expect(result).to.be.equal(
-        '{"width":1,"height":1, "enter":undefined, "exit":undefined, "room_count":0, "rooms":undefined, "world":undefined}',
+        '{"width":1,"height":1,"enter":{"x":0,"y":0,"room_id":0},"exit":{"x":0,"y":0,"room_id":0},"room_count":0,"rooms":{"0":{"neighbors":[],"doors":[],"top":0,"left":0,"width":0,"height":0,"deadend":true}},"world":[[]]}',
       );
     });
   });
 
   describe('fromJSON', () => {
     it('should turn string into Map', async () => {
-      const str = '{"width":1,"height":1}';
+      const str = `{"width":1,"height":1,"enter":{"x":0,"y":0,"room_id":0},"exit":{"x":0,"y":0,"room_id":0},"room_count":0,"rooms":{"0":{"neighbors":[],"doors":[],"top":0,"left":0,"width":0,"height":0,"deadend":true}},"world":[[]]}`;
       const result = Map.fromJSON(str);
 
-      expect(result).to.deep.equal({ width: 1, height: 1 });
-    });
-
-    it('should fall back to Zero vector when string is not valid', async () => {
-      const str = '{"width":"123","height":NaN}';
-      const result = Map.fromJSON(str);
-
-      expect(result.isNull).to.deep.equal(true);
+      expect(result).to.deep.equal({
+        width: 1,
+        height: 1,
+        enter: { x: 0, y: 0, room_id: 0 },
+        exit: { x: 0, y: 0, room_id: 0 },
+        room_count: 0,
+        rooms: { '0': { neighbors: [], doors: [], top: 0, left: 0, width: 0, height: 0, deadend: true } },
+        world: [[]],
+      });
     });
   });
 });
