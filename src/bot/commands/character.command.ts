091aead6b71ce4e8c356ad75a5205be29fabb295
Jon Deaves <hello@jondeaves.me>
2020-08-18 13:02:33 +0100

    chore: missed update after merge

--- src/bot/commands/character.command.ts
+++ src/bot/commands/character.command.ts
@@ -1,18 +1,60 @@
 import Discord from 'discord.js';
 
+import Vector2 from '../../core/helpers/Vector2';
+
 import Character from '../../carp/character/character.entity';
 
 import Command from './Command';
 
 export default class CharacterCommand extends Command {
-  async execute(message: Discord.Message): Promise<Discord.Message> {
-    // Just testing db stuff
+  public commandData: {
+    prefix: string;
+  };
+
+  async execute(message: Discord.Message, args: string[]): Promise<Discord.Message> {
     const matchedChar = await this.dependencies.databaseService.manager.findOne(Character, message.author.id);
 
-    if (!matchedChar) {
-      return message.reply(`Doesn't look like you have joined this campaign`);
+    if (args[0]) {
+      switch (args[0]) {
+        default:
+          if (matchedChar) {
+            return message.reply(`your character **${matchedChar.name}** is alive and well.`);
+          }
+          break;
+        case 'delete':
+          if (matchedChar) {
+            await this.dependencies.databaseService.manager.delete(Character, message.author.id);
+            return message.reply(`your character **${matchedChar.name}** has been deleted!`);
+          }
+          break;
+        case 'create':
+          if (!matchedChar) {
+            if (args[1]) {
+              const character = new Character();
+              const characterName = args[1];
+              character.name = characterName;
+              character.uid = message.author.id;
+              character.power = 1;
+              character.defense = 1;
+              character.max_health = 5;
+              character.current_health = 5;
+              character.position = Vector2.zero;
+              character.graphic = args[2] ?? `:slight_smile:`;
+              await this.dependencies.databaseService.manager.save(Character, character);
+              return message.reply(`that's it! You now have a character named **${args[1]}**!`);
+            }
+            return message.reply(`you're gonna have to give me a character name, too!`);
+          }
+          return message.reply(
+            `it looks like you're already set up with your character named **${matchedChar.name}**!`,
+          );
+      }
     }
-
-    return message.reply(`Welcome back ${matchedChar.name}`);
+    if (matchedChar) {
+      return message.reply(`your character **${matchedChar.name}** is alive and well.`);
+    }
+    return message.reply(
+      `it doesn't look like you have a character set up, yet. Run \`${this.commandData.prefix}character create <name>\` to get started.`,
+    );
   }
 }
